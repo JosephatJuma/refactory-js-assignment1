75fd20d2e6a807f5ec2bcedf634cef999e24f35d
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-02-21 16:20:24 +0300

    initial commit

---
+++ ternary.js
@@ -0,0 +1,15 @@
+//Constants decalaraion and initialization
+const myAge = 22;
+const grade = 18;
+//Ternary operations
+
+myAge >= 18 ? console.log("You are an adult") : console.log("you are young");
+//Ternary operation with a nested ternary operator
+grade <= 32
+  ? console.log("Good")
+  : grade <= 52
+  ? console.log("Fair")
+  : console.log("Poor Grades");
+
+//Ternary operation with a single condition
+grade <= 20 && console.log("Exellent");
