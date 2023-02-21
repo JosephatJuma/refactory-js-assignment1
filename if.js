515dd5eb117573e72856f328c4456c6b98cb666d
Klet2k <123959773+Klet2k@users.noreply.github.com>
2023-02-21 23:05:00 +0300

    Add files via upload

---
+++ if.js
@@ -0,0 +1,12 @@
+//If statement
+//conditional statement
+//Weather Conditions
+
+let weatherCondition = "Hot";
+let weatherChange = "Rainy";
+
+//if statement
+if(weatherCondition != weatherChange)
+{
+  console.log("IF BLOCK: Today is " +weatherChange);
+}
---
+++ ifelse.js
@@ -0,0 +1,10 @@
+let weatherCondition = "Hot";
+let weatherChange = "Hot";
+if(weatherCondition == weatherChange)
+{
+    console.log("IF ELSE BLOCK SUCCESS: Today is " +weatherCondition);
+}
+else
+{
+    console.log("IF ELSE BLOCK FAIL: Today is " +weatherChange);
+}
